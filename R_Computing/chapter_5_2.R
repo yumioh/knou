75128f4e56f5524eda053d474c4bd343a65cc7ef
yumi oh <iove0103@naver.com>
2024-04-05 18:03:29 +0900

    5강 강의 추가

--- R_Computing/chapter_5_2.R
+++ R_Computing/chapter_5_2.R
@@ -6,7 +6,7 @@ a <- 1:10
 b <- 11:15
 
 #리스트를 구성하는 성분의 이름
-klist <- list(vec1 =a, vec2 = b, descrip = "example") #리스트 생성
+klist <- list(vec1 = a, vec2 = b, descrip = "example") #리스트 생성
 klist
 
 #리스트의 성분 개수
@@ -16,3 +16,36 @@ mode(klist)
 #각 성분의 이름 출력
 names(klist)
 
+#리스트를 생성하고 추출하는 방법은 행렬과 배열 등과 유사 
+# - 특정한 성분을 추출하거나 성분 내의 특정 원소를 추출하려면 아래와 같은 연산자 사용
+# - [[]]를 사용하여 리스트에서의 성분을 추출, 성분의 이름이 부여되였다면 $연산자로 구분 
+
+list1 <- list("A", 1:8) #list 생성
+list1
+
+list1[[3]] <- list(c("T","F"))
+list1[[3]] 
+
+list1[[2]][9] <- 9
+list1
+
+list1[[3]] <- NULL
+
+list1
+
+list1[[2]] <- list1[[2]][-9]
+list1
+
+
+a <- c(1:10)
+b <- c(11:15)
+
+#변수에 example 문자 부여
+nlist <- list(vec1 = a, vec2 = b,descrip = "example")
+nlist
+
+nlist$vec1
+nlist$descrip
+
+nlist[[2]][5]
+nlist$vec2[c(2,3)]
---
+++ R_Computing/chapter_5_3.R
@@ -0,0 +1,56 @@
+#3. 데이터프레임
+# 데이터프레임은 열들이 서로 다른 형태의 객체를 가질 수 있음
+# 데이터 프레임은 형태(mode)가 일반화된 행렬(matrix)
+# 하나의 객체에 여러 종류의 자료가 들어갈 수 있음
+# 각 열은 각각 변소와 대응
+# 분석이나 모형 설정에 적합한 자료 객체
+
+#기본함수
+# read.table(file, header = FALSE, sep="") : 외부 텍스트 파일 부를 때
+# data.frame(객체1, 객체2...) : 여러 종류의 객체들을 서로 결함 (성격이 다른 데이터들)
+# as.data.frame(대상객체) : 다른 형태의 자료객체를 데이터 프레임의 형태로 변화 (수치형 행렬)
+# write.csv(대상객체) -> ,라 구분 / write.table(객체) -> sep =","을 주지 않으면 행부분이 공백으로 저장
+
+#예제 : read.table("d:stroy.txt", rownames = "num", header = T)
+#rownames : 데이터의 num이라는 변수가 행 이름
+#header = T : 첫번째줄이 변수명임을 의미
+
+char1 <- rep(LETTERS[1:3], c(2,2,1))
+#LETTERS : a,b,c 생성
+char1
+num1 <- rep(1:3,c(2,2,1))
+num1
+
+test1 <- data.frame(char1, num1)
+test1
+
+#예제 3-24
+a1 <- c("a","b","c","d","e","f","g","h","i","j","k","l","n","m","o")
+dim(a1) <- c(5,3)
+
+data1 <- as.data.frame(a1)
+data1
+
+#데이터프레임의 결합
+#cbind: 옆으로 합치기 , rbind: 아래로 합치기, merge : 병합하기
+
+#column bind : 옆으로 붙임
+cbind(test1, data1)
+
+
+char1 <- rep(LETTERS[1:3], c(1,2,2))
+#LETTERS : a,b,c 생성
+char1
+num1 <- rep(1:3,c(1,1,3))
+num1
+
+test2 <- data.frame(char1, num1)
+test2
+test1
+
+#변수명이 동일하야함 : 아래에 붙임
+rbind(test1, test2)
+
+merge(test1, data1)
+#같은 함수를 사용하면 같은 변수에 대해서 한번만 출력
+#알파벳 순서로 정렬되고 나머지 변수들은 옆으로 나열
