b4dff66c6b85b8cd66167950564c4d8b9b9437f7
yumioh <iove0103@naver.com>
2025-03-27 00:14:16 +0900

    중간과제물 완료

--- Regression/Midterm/Midterm_2.R
+++ Regression/Midterm/Midterm_2.R
@@ -29,7 +29,7 @@ summary(disp_lm)
 summary(hp_lm)
 
 # 2번문제 참고 코드
-model_multi <- lm(mpg ~ wt + disp + cyl + hp + vs + qsec, data = mtcars)
+model_multi <- lm(mpg ~ wt + disp + cyl + hp + vs + qsec + am + carb + gear, data = mtcars)
 model_multi
 
 
@@ -38,29 +38,55 @@ library(fmsb)
 
 # 2) 연비 예측에 추가적으로 도움이 되는 변수를 하나 더 추가하여 중회귀모형을 적합하고자 함
 #    가장 도움이 되는 추가 변수를 선정하고 근거를 제시하시오 
-# 종속변수(mpg)와의 상관관계가 높되, 기존 설명병수(wt)와는 상관관계가 낮아야 하는 변수를 찾아야함
+# [변수 선택 하는 방법]
+# - mpg와의 상관관계가 높을 것
+# - wt와는 다르게 mpg에 독립적인 설명력을 제공할 것 (공선성이 낮을 것)
+# - 다중회귀모형의 설명력을 실질적으로 높은 것
 
+# mpg 간 상관계수 확인 
+# cyl, disp는 상관계수가 높아 wt와 강한 상관관계가 있음
+# 나머지는 중간정도의 상관관계로 가능성이 있어 보임
+cor(mtcars$mpg, mtcars[, -1])
 
-# 변수 간 상관계수 확인
+# wt와 상관관계 확인
+# cyl, disp는 공성선이 높고, vs, qsec, am이 공선선이 낮거나 중간이라 선택 
 cor(mtcars$wt, mtcars$disp) # 강한 상관 관계
 cor(mtcars$wt, mtcars$cyl)  # 강한 상관 관계
 cor(mtcars$wt, mtcars$hp)   # 중간 상관 관계 
 cor(mtcars$wt, mtcars$vs)   # 중간 음의 상관관계 
-cor(mtcars$wt, mtcars$qsec) # 매우 낮은 상관관계 => 독립적
+cor(mtcars$wt, mtcars$drat) # 강한 음의 상관관계 
+cor(mtcars$wt, mtcars$am)   # 중간 음의 상관관계
+cor(mtcars$wt, mtcars$gear) # 중간 음의 상관관계
+cor(mtcars$wt, mtcars$carb) # 중간 양의 상관관계
+cor(mtcars$wt, mtcars$qsec) # 매우 낮은 상관관계
 
 # VIF(분산팽창지수) 해당하는 값이 높다면 강하게 상관관계가 있음 
 VIF(lm(mpg ~ wt + disp, data = mtcars))
 VIF(lm(mpg ~ wt + cyl, data = mtcars))
 VIF(lm(mpg ~ wt + hp, data = mtcars))
 VIF(lm(mpg ~ wt + vs, data = mtcars))
+VIF(lm(mpg ~ wt + am, data = mtcars))
+VIF(lm(mpg ~ wt + gear, data = mtcars))
+VIF(lm(mpg ~ wt + carb, data = mtcars))
 VIF(lm(mpg ~ wt + qsec, data = mtcars))
 
 # 전체 회귀식 p-value는 이 회귀모형은 무의미하다. → 즉, 모든 설명변수의 계수가 0이다.
 # 각 변수의 p-value는 해당 변수가 '종속변수(mpg)'에 선형적으로 영향을 주는가를 검정
+# hp, vs, am, qsec
 qsec_multi <- lm(mpg ~ wt + qsec, data = mtcars)
-disp_multi <- lm(mpg ~ wt + disp, data = mtcars)
 vs_multi <- lm(mpg ~ wt + vs, data = mtcars)
-summary(qsec_multi)
-summary(disp_multi)
+am_multi <- lm(mpg ~ wt + am, data = mtcars)
+hp_multi <- lm(mpg ~ wt + hp, data = mtcars)
 summary(vs_multi)
+summary(qsec_multi)
+summary(am_multi)
+summary(hp_multi)
 
+
+# 3)원래 주회귀모형의 회귀계수 해석, 표준화 회귀분석 결과및 해석
+#   표준화 해석이 적철치 않는 경우가 있다면 이유를 설명
+
+# 표준화 : 단위에 의존하지 않고 두 변수 중 어떤 변수가 상대적으로 영향력이 큰지 비교 가능
+lm(scale(mpg) ~ scale(wt) + scale(hp), data = mtcars)
+
+lm(scale(mpg) ~ scale(wt) + scale(qsec), data = mtcars)
